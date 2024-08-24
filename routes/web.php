ac2e9d880a7c09740e1329812f2fb6c7c053619b
shin0329 <shin.clover1209@icloud.com>
2024-08-24 04:53:57 +0000

    カリキュラム8-5

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use App\Http\Controllers\PostController;
 |
 */
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
 
 
 
